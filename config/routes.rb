4eb208b47ac8bc5c3f62066f09a8c4835af8aa29
AnnyaKri <sobakajustdoit@gmail.com>
2023-05-18 15:46:14 +0300

    added subscriptions, forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :comments, only: %i[ create destroy ]
   devise_for :users
-  resources :users, only: %i[ show edit update ]
-  resources :events
 
   root to: "events#index"
+
+  resources :events do
+    resources :comments, only: %i[ create destroy ]
+    resources :subscriptions, only: %i[ create destroy ]
+  end
+
+  resources :users, only: %i[ show edit update ]
+
 end
