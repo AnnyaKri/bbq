647f6b46a53c6287dfa9cb4c6a411879774376a6
AnnyaKri <sobakajustdoit@gmail.com>
2023-07-17 22:53:19 +0300

    fixed lightbox.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Entry point for the build script in your package.json
 import './map'
-import 'bs5-lightbox';
 import './lightbox'
 import "@hotwired/turbo-rails"
 import "./controllers"
--- app/javascript/lightbox.js
+++ app/javascript/lightbox.js
@@ -1,7 +1,10 @@
 import Lightbox from 'bs5-lightbox';
 
-document.querySelectorAll(".my-lightbox-toggle").forEach((el) => el.addEventListener("click", (e) => {
-    e.preventDefault();
-    const lightbox = new Lightbox(el, options);
-    lightbox.show();
-}));
+document.addEventListener('turbo:load', function (event){
+    document.querySelectorAll(".my-lightbox-toggle").forEach((el) => el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const lightbox = new Lightbox(el, options);
+      lightbox.show();
+    }))
+  }
+);
