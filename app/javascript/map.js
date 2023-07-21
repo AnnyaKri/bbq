65d4364b47846f66918563c87b24a0c6ffe9e72d
AnnyaKri <sobakajustdoit@gmail.com>
2023-07-21 21:42:52 +0300

    refactor map.js

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,31 +1,43 @@
-ymaps.ready(init);
+document.addEventListener('turbo:load', function (event) {
+  const mapElement = document.getElementById('map');
 
-function init() {
-    const address = document.getElementById('map').getAttribute('data-address');
+  if (!mapElement || mapElement.hasAttribute('data-transformed')) {
+    return;
+  }
+
+  ymaps.ready(init);
+
+  function init() {
+    const address = mapElement.getAttribute('data-address');
 
     const myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
+      center: [55.76, 37.64],
+      zoom: 10,
+      controls: []
     });
 
     const myGeocoder = ymaps.geocode(address);
 
     myGeocoder.then(
-        function (res) {
-            const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+      function (res) {
+        const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-            myMap.geoObjects.add(
-                new ymaps.Placemark(
-                    coordinates,
-                    {iconContent: address},
-                    {preset: 'islands#blueStretchyIcon'}
-                )
-            );
+        myMap.geoObjects.add(
+          new ymaps.Placemark(
+            coordinates,
+            {iconContent: address},
+            {preset: 'islands#blueStretchyIcon'}
+          )
+        );
 
-            myMap.setCenter(coordinates);
-            myMap.setZoom(15);
-        }, function (err) {
-            alert('Ошибка при определении местоположения');
-        }
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      },
+      function (err) {
+        alert('Ошибка при определении местоположения');
+      }
     );
-}
+
+    mapElement.setAttribute('data-transformed', true);
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: %i[ show edit update ]
-
 end
